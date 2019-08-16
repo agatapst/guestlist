1b98265c7bd2622369fe5043d831a44cba137d93
Agata Stawarz <agata.stawarz94@gmail.com>
2019-08-16 10:51:57 +0200

    Add initial project structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import GuestTable from './GuestTable'
+import AddGuestForm from './AddGuestForm'
 
-function App() {
+const App = () => {
+  const initialGuests = [
+    { id: 1, name: 'Kasia', details: 'with husband', menu: 'vegan'},
+    { id: 2, name: 'Basia', details: 'will be late', menu: 'classic' },
+  ]
+
+  const [guests, setGuests] = useState(initialGuests)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Wanna invite somebody?</h1>
+      <AddGuestForm />
+      <h1>Your Guests:</h1>
+      <GuestTable guestsList={guests} />  
     </div>
-  );
+  )
 }
 
 export default App;
