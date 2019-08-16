d36c237609a1370309a63af12a6586df3c405884
Agata Stawarz <agata.stawarz94@gmail.com>
2019-08-16 12:42:36 +0200

    Add adding new guests feature

--- src/AddGuestForm.js
+++ src/AddGuestForm.js
@@ -1,15 +1,28 @@
 import React, { useState } from 'react'
 
 const AddUserForm = props => {
+    const InitialGuestForm = {id: null, name: '', details: '', menu: ''}
+    const [guest, setGuest] = useState(InitialGuestForm)
+
+    const handleInputChange = event => {
+        const { name, value } = event.target
+        setGuest({ ...guest, [name]: value })
+      }
 
   return (
-    <form>
+    <form onSubmit={event => {
+        event.preventDefault()
+        if (!guest.name) return
+
+        props.addGuest(guest)
+        setGuest(InitialGuestForm)
+      }}>
       <label>Name</label>
-      <input type="text" name="name"/>
+      <input type="text" name="name" value={guest.name} onChange={handleInputChange}/>
       <label>Details</label>
-      <input type="text" name="details"/>
+      <input type="text" name="details" value={guest.details} onChange={handleInputChange}/>
       <label>Menu</label>
-      <input type="text" name="menu"/>
+      <input type="text" name="menu" value={guest.menu} onChange={handleInputChange}/>
       <button>Add new guest</button>
     </form>
   )
--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import GuestTable from './GuestTable'
 import AddGuestForm from './AddGuestForm'
 
 const App = () => {
-  const initialGuests = [
+  const InitialGuests = [
     { id: 1, name: 'Kasia', details: 'with husband', menu: 'vegan'},
     { id: 2, name: 'Basia', details: 'will be late', menu: 'classic' },
   ]
 
-  const [guests, setGuests] = useState(initialGuests)
+  const [guests, setGuests] = useState(InitialGuests)
+
+  const addNewGuest = newGuest => {
+    newGuest.id = guests.length + 1
+    setGuests([...guests, newGuest])
+  }
 
   return (
     <div>
       <h1>Wanna invite somebody?</h1>
-      <AddGuestForm />
+      <AddGuestForm addGuest={addNewGuest} />
       <h1>Your Guests:</h1>
       <GuestTable guestsList={guests} />  
     </div>
